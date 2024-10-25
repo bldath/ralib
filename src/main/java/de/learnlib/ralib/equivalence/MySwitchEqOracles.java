1f7fea8ad8b6a28349ce01e07705234f8a0958b4
bldath <blida.dagsdottir@gmail.com>
2024-10-25 03:57:45 +0200

    Small fix in draft

--- src/main/java/de/learnlib/ralib/equivalence/MySwitchEqOracles.java
+++ src/main/java/de/learnlib/ralib/equivalence/MySwitchEqOracles.java
@@ -10,6 +10,8 @@ import de.learnlib.ralib.words.PSymbolInstance;
 public class MySwitchEqOracles implements IOEquivalenceOracle{
     private IOEquivalenceOracle raOracle;
     private IOEquivalenceOracle raMealyOracle;
+    private Boolean useRAEqOracle = false;
+    DefaultQuery<PSymbolInstance, Boolean> counterexample = null;
 
 
     public MySwitchEqOracles(IOEquivalenceOracle raOracle, IOEquivalenceOracle raMealyOracle) {
@@ -19,10 +21,23 @@ public class MySwitchEqOracles implements IOEquivalenceOracle{
 
     @Override
     public DefaultQuery<PSymbolInstance, Boolean> findCounterExample(RegisterAutomaton ra, Collection<? extends PSymbolInstance> clctn) {
-        DefaultQuery<PSymbolInstance, Boolean> counterexample = raMealyOracle.findCounterExample(ra, clctn);
-        if (counterexample == null) {
-            counterexample = raOracle.findCounterExample(ra, clctn);
+        if (!useRAEqOracle) {
+            counterexample = raMealyOracle.findCounterExample(ra, clctn);
+            if (counterexample == null) {
+                useRAEqOracle = true;
+                counterexample = raOracle.findCounterExample(ra, clctn);
+            }
+        } else {
+            counterexample = raMealyOracle.findCounterExample(ra, clctn);
+            if (counterexample == null) {
+                //useRAEqOracle = false;
+            }
         }
         return counterexample;
     }
+
+    public void useRAEqOracleOrNot(Boolean b) {
+        this.useRAEqOracle = b;
+    }
+
 }
