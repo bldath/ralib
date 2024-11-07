d8c644e46c628a218c64eff5615efd891d958961
bldath <blida.dagsdottir@gmail.com>
2024-11-07 12:24:14 +0100

    Some more progress on Monte Carlo

--- MyMonteCarlo.sh
+++ MyMonteCarlo.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
-# From linux command line:
+# From linux command line, run:
 # chmod 755 MyMonteCarlo.sh
 
+# To clean, run:
+# rm ce_count.txt ce_max.txt ce_avg.txt testing.txt output.txt
 
-java -ea -jar target/ralib-0.1-SNAPSHOT-jar-with-dependencies.jar iosimulator -f config > output.txt
-cat output.txt | grep -F 'Counterexamples' | grep -o -E '[0-9]+'>> ce_count.txt
-cat output.txt | grep -F 'CE max length' | grep -o -E '[0-9]+'>> ce_max.txt
-cat output.txt | grep -F 'CE avg length' | grep -o -E '[0-9]+'>> ce_avg.txt
-cat output.txt | grep -F 'Testing'>> testing.txt
+n=1
+
+while [ $n -le 9 ]
+do
+    java -ea -jar target/ralib-0.1-SNAPSHOT-jar-with-dependencies.jar iosimulator -f config > output.txt
+    cat output.txt | grep -F 'Counterexamples' | grep -o -E '[0-9]+'>> ce_count.txt
+    cat output.txt | grep -F 'CE max length' | grep -o -E '[0-9]+'>> ce_max.txt
+    cat output.txt | grep -F 'CE avg length' | grep -o -E '[0-9]+'>> ce_avg.txt
+    cat output.txt | grep -F 'Testing'>> testing.txt
+    n=$(($n+1))
+done
